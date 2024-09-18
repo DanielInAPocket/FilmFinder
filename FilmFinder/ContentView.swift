c67abb80b2b84112cf5946dbc110e855ed4d2c3f
Daniels Klinge <parparis888@gmail.com>
2024-09-18 12:48:24 +0300

    Initial commit

---
+++ FilmFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FilmFinder
+//
+//  Created by Daniel Klinge on 16/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FilmFinder/Resources/FilmFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmFinderApp.swift
+//  FilmFinder
+//
+//  Created by Daniel Klinge on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FilmFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
